f851faf977213a5a78ba3f56cb9cc6f229218eb4
Walt Ritscher <waltr@scandiasoft.com>
2017-02-02 23:36:08 -0800

    Add Tip 001

---
+++ ConsoleApp/Tip001.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyTips.ConsoleApp {
+	class Tip001 {
+
+		// Brace matching
+		// Change default colors for braces
+
+		public void Demo() {
+
+			// comment
+			// another comment
+
+
+			var wholesaleCost = 45.89M;
+
+			Console.WriteLine(wholesaleCost);
+
+			var markup = .30M;
+			var retail = wholesaleCost *= markup;
+			var oddNumbers = new int[1, 3, 5, 7, 9];
+			var evenNumbers = new List<int>();
+
+			if (markup > .5M
+					&& wholesaleCost > 2000)
+			{
+
+			}
+		}
+
+
+	}
+}
+
+
+// More info
+// From the LinkedIn Learning / Lynda.com course
+// Visual Studio Weekly Tips.
+//
+//	See more Visual Studio Courses at https://lynda.com/waltritscher
+//  twitter: @waltritscher
