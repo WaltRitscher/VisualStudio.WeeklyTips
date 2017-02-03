d80f7ca85af9fceca3814530ad5322e4279d3813
Walt Ritscher <waltr@scandiasoft.com>
2017-02-02 22:42:36 -0800

    Change Namespaces to WeeklyTips.
    Added WPF project

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp {
+namespace WeeklyTips.ConsoleApp {
 	class Program {
 		static void Main(string[] args) {
 		}
