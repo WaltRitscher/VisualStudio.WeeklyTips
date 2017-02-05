9dc81e169ec692e12b9427071cfbba633b7975cb
Walt Ritscher <waltr@scandiasoft.com>
2017-02-05 00:39:24 -0800

    Added Tip003

---
+++ ConsoleApp/Tip003.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyTips.ConsoleApp {
+	class Tip003 {
+
+		public void Demo() {
+			// Move code
+
+		}
+
+	}
+}
