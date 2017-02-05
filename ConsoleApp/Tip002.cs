d557022da9360257fcb8d127982b3cf72abbe40a
Walt Ritscher <waltr@scandiasoft.com>
2017-02-05 00:08:42 -0800

    Add Tip002

---
+++ ConsoleApp/Tip002.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyTips.ConsoleApp {
+	class Tip002 {
+
+		public void Demo() {
+			// box selection 
+
+		}
+		}
+}
