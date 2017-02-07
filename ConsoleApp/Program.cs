3216cd19160b2be8da56ccf8070f7ff1662f566f
Walt Ritscher <waltr@scandiasoft.com>
2017-02-06 19:27:06 -0800

    Add code to Tips 2 and 4

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace WeeklyTips.ConsoleApp {
 	class Program {
 		static void Main(string[] args) {
+
+			// Run the example from the main method.
+
+			var tip = new Tip003();
+			tip.RunDemo_MoveCode();
+			Console.ReadLine();
 		}
 	}
 }
