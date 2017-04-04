d239c5ad1870ec2b31c5273da4122b00d1864fce
WR <WR@PRIMROSE>
2017-04-04 00:42:06 -0700

    Add PasteHere class.

---
+++ CourseLib/PasteHere.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib {
+	class PasteHere {
+
+		// use the clipboard ring
+		// and paste the result here...
+
+
+	}
+}
