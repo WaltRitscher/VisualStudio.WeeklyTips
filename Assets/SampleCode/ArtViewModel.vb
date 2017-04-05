d7ae87f1843c0dbc980ce51629bc6d03c82ccba2
Walt Ritscher <waltritscher@outlook.com>
2017-04-04 21:35:04 -0700

    Add code samples to Assets project

---
+++ Assets/SampleCode/ArtViewModel.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+
+Namespace Models
+	Public Class ArtViewModel
+
+		Public Property ArtProjects() As List(Of ArtProject)
+	End Class
+End Namespace
---
+++ Assets/SampleCode/ToursViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+	public class ToursViewModel
+	{
+
+		public List<Tour> Tours { get; set; }
+	}
+}
