45333fe6278b5fc2f966e83232ffbcf1cedb0025
Walt Ritscher <waltritscher@outlook.com>
2017-04-04 23:42:15 -0700

    Add reference to CourseLib Add code to show the TourStops.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourseLib.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,12 @@ namespace WeeklyTips.ConsoleApp {
 	class Program {
 		static void Main(string[] args) {
 
-			// Run the example from the main method.
+      // Run the example from the main method.
 
+      foreach (TourStop  stop  in CourseLib.Models.TourSource.TourStops)
+      {
+        Console.WriteLine($"Stop Number: {stop.StopNumber}, Stop Name: {stop.Name}");
+      }
 
 		}
 	}
