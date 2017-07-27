d604cad0b0fbff48ab344e5a247f40d0e77a071e
Walt Ritscher <waltritscher@outlook.com>
2017-07-26 23:09:51 -0700

    Remove DateTime extension code Add sample code for remove whitespace

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -11,17 +11,10 @@ namespace WeeklyTips.ConsoleApp {
 		public void RunExample() {
 			// write the example code here...
 
-			var yesterday = DateTime.Now.Yesterday();
-			var tomorrow = DateTime.Now.Tomorrow();
+			String example;
+			example = "Sample text for code demo.";
 
-			var futureDate1 = DateTime.Now.AddDays(12);
-			var futureDate2 = DateTime.Now.AddWeekdays(12);
-
-			var firstDate = DateTime.Now.FirstDayOfMonth();
-			var lastDay = DateTime.Now.LastDayOfMonth();
-
-
-			var roundedMinute = DateTime.Now.Round(DateTimeExtensions.RoundUnit.Minute);
+			Console.WriteLine(example);  
 		}
 	}
 
