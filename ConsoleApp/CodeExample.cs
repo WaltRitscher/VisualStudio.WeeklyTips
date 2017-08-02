287e3711b9b218275f50d91dfb8c4199e4dbd91b
WR <WR@PRIMROSE>
2017-08-01 20:01:42 -0700

    Clean up code

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -6,20 +6,18 @@ using System.Threading.Tasks;
 
 namespace WeeklyTips.ConsoleApp
 {
-  class CodeExample
-  {
+	class CodeExample
+	{
 
-    public static void DoWork()
-    {
-      // write the example code here...
+		public static void DoWork()
+		{
+			// write the example code here...
 
-      var smallDoubleArray = new double[5] { -2.0, -1.0, 0.0, 1.0, 2.0 };
+			var smallDoubleArray = new double[5] { -2.0, -1.0, 0.0, 1.0, 2.0 };
+			var smallIntArray = new int[] { 2, 4, 6, 9, 18, 27, 16, 8 };
 
+			var board = new int[2, 5] { { 1, 1, 2, 3, 4 }, { 14, 21, 28, 35, 28 } };
 
-      var smallIntArray = new int[] { 2, 4, 6, 9, 18, 27, 16, 8 };
-
-      var board = new int[2, 5] { { 1, 1, 2, 3, 4 }, { 14, 21, 28, 35, 28 } };
-
-    }
-  }
+		}
+	}
 }
