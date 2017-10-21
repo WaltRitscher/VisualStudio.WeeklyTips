52fc4ad74de877cec33487e31e4169d8d3f13137
Walt Ritscher <waltritscher@outlook.com>
2017-10-21 14:52:19 -0700

    Copy of original demo

---
+++ CommentRemover/CommentRemoverConsole/Tour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentRemoverConsole {
+  class Tour {
+    #region Properties
+
+    public string TourName { get; set; }
+    public string Description { get; set; }
+		#endregion
+
+
+		#region Constructors
+		public Tour(string tourName) {
+			// constructor
+			// setup the properties
+			TourName = tourName;
+		}
+   
+    /// <summary>
+    /// Constructor with two parameters
+    /// </summary>
+    /// <param name="tourName">The TourName </param>
+    /// <param name="description">The Description</param>
+    public Tour(string tourName, string description) {
+			// constructor
+      // setup the properties
+      TourName = tourName;
+      Description = description;
+    }
+		#endregion
+	}
+}
