2b086447b5b64af21eb9939cf9884b8409f3e902
Walt Ritscher <waltritscher@outlook.com>
2017-10-21 15:35:56 -0700

    Initial project files

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -11,25 +11,10 @@ namespace WeeklyTips.WpfApp
     public MainWindow()
     {
       InitializeComponent();
-      this.Loaded += MainWindow_Loaded;
     }
 
-    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-    {
-      SolidSlider1.ValueChanged += Slider_ValueChanged;
-      SolidSlider2.ValueChanged += Slider_ValueChanged;
-      SpaceSlider1.ValueChanged += Slider_ValueChanged;
-      SpaceSlider2.ValueChanged += Slider_ValueChanged;
-    }
 
-    private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-    {
-      DoubleCollection nums = new DoubleCollection();
-      nums.Add(SolidSlider1.Value);
-      nums.Add(SpaceSlider1.Value);
-      nums.Add(SolidSlider2.Value);
-      nums.Add(SpaceSlider2.Value);
-      mainRect.StrokeDashArray = nums;
-    }
+
+
   }
 }
