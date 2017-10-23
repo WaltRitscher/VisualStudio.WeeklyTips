05bec558b90998b8faab24dae3aef248a09a8de9
Walt Ritscher <waltritscher@outlook.com>
2017-10-22 18:22:19 -0700

    Add LINQ code

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -11,10 +11,24 @@ namespace WeeklyTips.WpfApp
     public MainWindow()
     {
       InitializeComponent();
+      this.Loaded += MainWindow_Loaded;
     }
 
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      GetSource();
+    }
 
+    private void GetSource()
+    {
+      var source = new SalesDataSource();
+      Sales1.DataContext = source.GetRandomized();
+      Sales2.DataContext = source.GetRandomized();
+    }
 
-
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      GetSource();
+    }
   }
 }
---
+++ WpfApp/SalesDataSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyTips.WpfApp
+{
+  internal class SalesDataSource
+  {
+
+    ObservableCollection<int> _sales = new ObservableCollection<int>();
+    Random _rand = new Random();
+   internal SalesDataSource()
+    {
+      _sales.Add(100);
+      _sales.Add(200);
+      _sales.Add(300);
+      _sales.Add(400);
+      _sales.Add(500);
+      _sales.Add(600);
+      _sales.Add(700);
+    }
+
+    internal ObservableCollection<int> GetAll()
+    {
+      return _sales;
+    }
+    internal ObservableCollection<int> GetRandomized()
+    {
+      var result = new ObservableCollection<int>( _sales.OrderBy(x => _rand.Next()).ToList() );
+      return result;
+    }
+    internal ObservableCollection<int> GetRandomized2()
+    {
+      var q = from s in _sales
+              orderby _rand.Next()
+              select s;
+
+      return new ObservableCollection<int> (q.ToList());
+    }
+  }
+}
