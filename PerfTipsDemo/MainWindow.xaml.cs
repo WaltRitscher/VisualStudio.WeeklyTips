4dfb70e8c3a94925e1e004e6bef8e6d0ebca4d26
Walt Ritscher <waltritscher@outlook.com>
2017-10-23 21:53:02 -0700

    Add PerfTips project

---
+++ PerfTipsDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PerfTipsDemo {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private async void Button_Click(object sender, RoutedEventArgs e) {
+			var nameGen = new NameGeneratorLib.NameGenerator();
+
+			var maleName = nameGen.GetMaleName();
+      var maleNames = await nameGen.GetMaleNames(8);
+      var femaleNames = await nameGen.GetFemaleNames(8);
+
+      MaleNamesListBox.ItemsSource = maleNames;
+      FemaleNamesListBox.ItemsSource = femaleNames;
+
+    }
+
+
+  }
+}
