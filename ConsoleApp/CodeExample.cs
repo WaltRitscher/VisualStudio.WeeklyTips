3a9f928c19fa3a8d84c7194162ed13ba4d231483
Staff Author <staffauthor@outlook.com>
2017-10-25 00:33:44 -0700

    Clean up code

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -55,8 +55,6 @@ namespace WeeklyTips.ConsoleApp
         {
           if (tour.TourName != null)
           {
-
-
             if (tour.TourStops != null)
             {
               if (tour.TourStops.Count>0)
@@ -99,34 +97,7 @@ namespace WeeklyTips.ConsoleApp
 
     }
   }
-  public class Tour
-  {
+ 
 
-    public Tour()
-    {
-      _tourStops = new List<TourStop>();
-    }
-    public string TourName { get; set; }
-
-    private IList<TourStop> _tourStops;
-
-    public IList<TourStop> TourStops {
-      get { return _tourStops; }
-      set { _tourStops = value; }
-    }
-
-
-
-  }
-  public class TourStop
-  {
-
-    public int StopNumber { get; set; }
-    public string ImageUrl { get; set; }
-    public string StopName { get; set; }
-    public string Latitude { get; set; }
-    public string Longitude { get; set; }
-    public string Description { get; set; }
-    public string Phone { get; set; }
-  }
+   
 }
---
+++ ConsoleApp/Tours.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyTips.ConsoleApp
+{
+
+	public class Tour
+	{
+		public Tour()
+		{
+			_tourStops = new List<TourStop>();
+		}
+		public string TourName { get; set; }
+
+		private IList<TourStop> _tourStops;
+
+		public IList<TourStop> TourStops {
+			get { return _tourStops; }
+			set { _tourStops = value; }
+		}
+
+	}
+	public class TourStop
+	{
+
+		public int StopNumber { get; set; }
+		public string ImageUrl { get; set; }
+		public string StopName { get; set; }
+		public string Latitude { get; set; }
+		public string Longitude { get; set; }
+		public string Description { get; set; }
+		public string Phone { get; set; }
+	}
+}
