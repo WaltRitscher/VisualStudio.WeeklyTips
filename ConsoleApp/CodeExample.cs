acef63c66b31ab4654f7f328682dc0d97755868b
Walt Ritscher <waltritscher@outlook.com>
2017-10-24 21:40:06 -0700

    Add Null prop examples

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -5,24 +5,128 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace WeeklyTips.ConsoleApp {
-	class CodeExample {
+namespace WeeklyTips.ConsoleApp
+{
+  class CodeExample
+  {
 
-		public void RunExample() {
-			// write the example code here...
+    public void RunExample()
+    {
+      var tour1 = new Tour();
+      var tour2 = new Tour();
+      var tour3 = new Tour();
+      tour2.TourName = "Riverwalk at Night";
 
-			//var firstDate = DateTime.Now.FirstDayOfMonth();
-			//var lastDay = DateTime.Now.LastDayOfMonth();
+      tour3.TourName = "Best Buskers in Belltown";
+      tour3.TourStops.Add(new TourStop { StopName = "Mordy's Magic" });
+      tour3.TourStops.Add(new TourStop { StopName = "Piccolo and Tuba band" });
+      tour3.TourStops.Add(new TourStop { StopName = "Carnival songs with Aya" });
 
-			var futureDate1 = DateTime.Now.AddDays(12);
-			var futureDate2 = DateTime.Now.AddWeekdays(12);
-
-			
-
-
-			var roundedMinute = DateTime.Now.Round(DateTimeExtensions.RoundUnit.Minute);
-		}
-	}
+      PrintStopName(null);
+      PrintStopName(tour1);
+      PrintStopName(tour2);
+      PrintStopName(tour3);
+      Console.ReadLine();
+    }
 
 
+
+    public void PrintStopName(Tour tour)
+    {
+
+      try
+      {
+        Console.WriteLine($"Tour: {tour.TourName}, Stop:{tour.TourStops.FirstOrDefault().StopName}");
+      }
+      catch (Exception ex)
+      {
+
+        Console.WriteLine($"Exception: {ex.Message}");
+      }
+
+    }
+
+    public void PrintStopName2(Tour tour)
+    {
+
+      try
+      {
+        if (tour != null)
+        {
+          if (tour.TourName != null)
+          {
+
+
+            if (tour.TourStops != null)
+            {
+              if (tour.TourStops.Count>0)
+              {
+                Console.WriteLine($"Tour: {tour.TourName}, Stop:{tour.TourStops.FirstOrDefault().StopName}");
+              }
+              
+            }
+          }
+
+        }
+
+
+      }
+      catch (Exception ex)
+      {
+
+        Console.WriteLine($"Exception: {ex.Message}");
+      }
+
+    }
+
+    public void PrintStopName3(Tour tour)
+    {
+
+      try
+      {
+
+        {
+          Console.WriteLine($"Tour: {tour?.TourName}, Stop:{tour?.TourStops?.FirstOrDefault()?.StopName}");
+        }
+
+
+      }
+      catch (Exception ex)
+      {
+
+        Console.WriteLine($"Exception: {ex.Message}");
+      }
+
+    }
+  }
+  public class Tour
+  {
+
+    public Tour()
+    {
+      _tourStops = new List<TourStop>();
+    }
+    public string TourName { get; set; }
+
+    private IList<TourStop> _tourStops;
+
+    public IList<TourStop> TourStops {
+      get { return _tourStops; }
+      set { _tourStops = value; }
+    }
+
+
+
+  }
+  public class TourStop
+  {
+
+    public int StopNumber { get; set; }
+    public string ImageUrl { get; set; }
+    public string StopName { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    public string Description { get; set; }
+    public string Phone { get; set; }
+  }
 }
