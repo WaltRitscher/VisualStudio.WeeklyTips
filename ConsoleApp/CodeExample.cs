dbe71791bf8aef97da901583d75230309de490c8
Walt Ritscher <waltritscher@outlook.com>
2018-03-25 13:15:31 -0700

    Add Trace output to the example

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace WeeklyTips.ConsoleApp {
 	class CodeExample {
 
 		public void RunExample() {
-			// write the example code here...
+            // write the example code here...
+            Trace.WriteLine("==================="); 
+            Trace.WriteLine("Trace Ready...");
 
-		}
+            Trace.WriteLine("===================");
+            Trace.WriteLine("Debug Ready...");
+        }
 	}
 
 
