839a6b9d7989bfa0e3d2b7653b9af6e00aa0172d
Staff Author <staffauthor@outlook.com>
2018-03-28 17:15:32 -0700

    add custom class type for List<T>

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -5,22 +5,22 @@ using System.Linq;
 using System.Collections.Generic;
 namespace WeeklyTips.ConsoleApp
 {
-  internal class CodeExample
-  {
-    public void RunExample()
-    {
-      // write the example code here...
-     
-      // SequenceEquals returns true if both lists contain the same items.
-      var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 22 };
-      var evenNumbers = new List<int> { 2, 4, 6, 8 };
-      var oddNumbers = new List<int> { 1, 3, 5, 7, 9 };
-      var userSelectedNumbers = new List<int> { 2, 4, 6, 8 };
+	internal class CodeExample
+	{
+		public void RunExample()
+		{
+			// write the example code here...
 
-      bool areSame;
+			// SequenceEquals returns true if both lists contain the same items.
+			var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 22 };
+			var evenNumbers = new List<int> { 2, 4, 6, 8 };
+			var oddNumbers = new List<int> { 1, 3, 5, 7, 9 };
+			var userSelectedNumbers = new List<int> { 2, 4, 6, 8 };
 
-      areSame = evenNumbers == userSelectedNumbers;
-      Console.WriteLine($"evenNumbers == userSelectedNumbers: {areSame}");
+			bool areSame;
+
+			areSame = evenNumbers == userSelectedNumbers;
+			Console.WriteLine($"evenNumbers == userSelectedNumbers: {areSame}");
 
 			//areSame = evenNumbers.SequenceEqual(userSelectedNumbers);
 			//Console.WriteLine($"evenNumbers.SequenceEqual(userSelectedNumbers): {areSame}");
@@ -35,7 +35,34 @@ namespace WeeklyTips.ConsoleApp
 
 			//diff.ForEach(x=>Console.WriteLine(x) );
 
+			#region Create tours
+			var bikeTour = new Tour
+			{
+				TourName = "Bikes in Barcelona",
+				AvailableDays = Days.Monday,
+				AvailableMonths = Months.April
+			};
+
+			var dupTour = new Tour
+			{
+				TourName = "Bikes in Barcelona",
+				AvailableDays = Days.Monday,
+				AvailableMonths = Months.April
+			};
+			var sailingTour = new Tour
+			{
+				TourName = "Deserted Islands",
+				AvailableDays = Days.Saturday,
+				AvailableMonths = Months.August
+			};
+			#endregion
+
+			var tourList1 = new List<Tour> {bikeTour, sailingTour };
+			var tourList2 = new List<Tour> { dupTour, sailingTour };
+
+			areSame = tourList1.SequenceEqual(tourList2);
+			Console.WriteLine($"tourList1.SequenceEqual(tourList2): {areSame}");
 			Console.ReadLine();
-    }
-  }
+		}
+	}
 }
