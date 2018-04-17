06063d855ff9628f07760ef048927d56603e00c6
Walt Ritscher <waltritscher@outlook.com>
2018-04-16 19:13:55 -0700

    Add Venn diagram images, clean up demo code, add regions

--- LinqSetOperators/MainWindow.xaml.cs
+++ LinqSetOperators/MainWindow.xaml.cs
@@ -35,34 +35,62 @@ namespace LinqSetOperators {
 
     private void UnionButton_Click(object sender, RoutedEventArgs e) {
       ResultListBox.DataContext = setA.Union(setB).ToList();
+
+      #region UpdateUI
       MessageTextBlock.Text = "Union: (A-B) all numbers in both sets - no duplicates.";
+      Uri packUri = new Uri("Images/VennUnion.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
+    }
+
+   
+
+    private void Union2Button_Click(object sender, RoutedEventArgs e) {
+      ResultListBox.DataContext = setA.Union(setC).ToList();
+      #region UpdateUI
+      MessageTextBlock.Text = "Union: (A-C) all numbers in both sets - no duplicates.";
+      Uri packUri = new Uri("Images/VennUnion.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
+
     }
 
     private void IntersectButton_Click(object sender, RoutedEventArgs e) {
       ResultListBox.DataContext = setA.Intersect(setB).ToList();
+      #region UpdateUI
       MessageTextBlock.Text = "Intersect: (A-B) unique items that occur in both sets.";
+      Uri packUri = new Uri("Images/VennIntersection.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
 
     }
-
-    private void Union2Button_Click(object sender, RoutedEventArgs e) {
-      ResultListBox.DataContext = setA.Union(setC).ToList();
-      MessageTextBlock.Text = "Union: (A-C) all numbers in both sets - no duplicates.";
-
-    }
-
     private void Intersect2Button_Click(object sender, RoutedEventArgs e) {
       ResultListBox.DataContext = setA.Intersect(setC).ToList();
+      #region UpdateUI
       MessageTextBlock.Text = "Intersect: (A-C) unique items that occur in both sets.";
+
+      Uri packUri = new Uri("Images/VennIntersection.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
     }
 
     private void ExceptButton_Click(object sender, RoutedEventArgs e) {
       ResultListBox.DataContext = setA.Except(setB).ToList();
+      #region UpdateUI
       MessageTextBlock.Text = "Except: (A-B)  items in first source that do not occur in second source.";
+      Uri packUri = new Uri("Images/VennExcept.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
     }
 
     private void Except2Button_Click(object sender, RoutedEventArgs e) {
       ResultListBox.DataContext = setA.Except(setC).ToList();
+
+      #region UpdateUI
       MessageTextBlock.Text = "Except: (A-C) items in first source that do not occur in second source.";
+      Uri packUri = new Uri("Images/VennExcept.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
     }
 
     private void Cartesian1Button_Click(object sender, RoutedEventArgs e) {
@@ -71,20 +99,32 @@ namespace LinqSetOperators {
               select $"{a}, {b} ";
 
       ResultListBox.DataContext = q.ToList();
+      #region UpdateUI
       MessageTextBlock.Text = "Cartesian: (A-B) produces every possible pairing between the items from both sets.";
+      Uri packUri = new Uri("Images/VennCartesian.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
 
     }
 
     private void Cartesian2Button_Click(object sender, RoutedEventArgs e) {
       var q = setA.SelectMany(x => setC.Select(y => $"{x}, {y} "));
       ResultListBox.DataContext = q.ToList();
+      #region UpdateUI
       MessageTextBlock.Text = "Cartesian: (A-C) produces every possible pairing between the items from both sets.";
+      Uri packUri = new Uri("Images/VennCartesian.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri);
+#endregion
     }
 
     private void Cartesian3Button_Click(object sender, RoutedEventArgs e) {
       var q = setA.SelectMany(x => setB.Select(y => $"{x}x{y}={x*y} "));
       ResultListBox.DataContext = q.ToList();
+      #region UpdateUI
       MessageTextBlock.Text = "Times Table:";
+      Uri packUri = new Uri("Images/VennCartesian.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri);
+      #endregion
     }
 
     private void Cartesian4Button_Click(object sender, RoutedEventArgs e) {
@@ -94,7 +134,11 @@ namespace LinqSetOperators {
               select $"{a},{b},{c}";
 
       ResultListBox.DataContext = q.ToList();
+      #region UpdateUI
       MessageTextBlock.Text = "Cartesian: (three sets)";
+      Uri packUri = new Uri("Images/VennCartesian.png", UriKind.RelativeOrAbsolute);
+      VennImage.Source = new BitmapImage(packUri); 
+      #endregion
     }
   }
 }
