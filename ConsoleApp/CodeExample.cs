12635ead934771b778929b08b39ec6df380a2d44
Walt Ritscher <waltritscher@outlook.com>
2018-04-22 23:04:35 -0700

    Formatting  and spacing

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -10,14 +10,13 @@ namespace WeeklyTips.ConsoleApp {
 
       // this is big number,  
       // 998,877,665,544,332,211
-
-
+      
       long bigNumber = 998877665544332211;
 
       Console.WriteLine(bigNumber);
       Console.WriteLine(bigNumber.ToString("N")); // uses default culture for separators
 
-      Console.WriteLine(bigNumber.ToString("N", System.Globalization.CultureInfo.GetCultureInfo("DE-de"))); // uses non english culture for separators
+      Console.WriteLine(bigNumber.ToString("N", System.Globalization.CultureInfo.GetCultureInfo("DE-de"))); // uses german culture for separators
 
       #region Digit Separator
       //// Improve readability with digit separator
@@ -28,9 +27,6 @@ namespace WeeklyTips.ConsoleApp {
 
       //Console.WriteLine(bigNumber); 
       #endregion
-
-
-
 
 
       #region Binary Literals
