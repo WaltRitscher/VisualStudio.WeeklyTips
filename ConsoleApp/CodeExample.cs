5966d1db949f40199e4dca6ee8ec12213fd3a07c
Walt Ritscher <waltritscher@outlook.com>
2018-04-22 23:06:58 -0700

    Remove duplicate namespace

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-using System.Collections.Generic;
+
 namespace WeeklyTips.ConsoleApp {
   internal class CodeExample {
     public void RunExample() {
