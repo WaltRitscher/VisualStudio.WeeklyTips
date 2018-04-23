6ead9cbb8962a02d32c364cc97a70763bf11d01e
Staff Author <staffauthor@outlook.com>
2018-04-22 23:16:24 -0700

    Fix typo Add line break at end of example

--- ConsoleApp/CodeExample.cs
+++ ConsoleApp/CodeExample.cs
@@ -11,19 +11,19 @@ namespace WeeklyTips.ConsoleApp {
       // this is one way to format a string with line breaks.
       // poem from Francis William Bourdillon - 1852
 
-      var poem1 = "The night has a thousand eyes.\r\nAnd the day but one,\r\nYet the light of the bright world dies\r\nWith the dying sun.";
+      var poem1 = "The night has a thousand eyes.\r\nAnd the day but one,\r\nYet the light of the bright world dies\r\nWith the dying sun.\r\n";
       Console.WriteLine(poem1);
 
       
       #region verbatim string
-       // use the @ symbol to create verbatim string, which means the string content contains line feeds charactors.
+       // use the @ symbol to create verbatim string, which means the string content contains line feeds characters.
       var poem2 =
- @"The night has a thousand eyes.
+			 @"The night has a thousand eyes.
 And the day but one,
 Yet the light of the bright world dies
 With the dying sun.";
 
-      Console.WriteLine(poem2);
+     // Console.WriteLine(poem2);
 
       #endregion
 
